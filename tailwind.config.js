e62fdc84ecd48a8d3d3d5ca29316601d4cb924d0
andrewflbarnes <andrewflbarnes@hotmail.co.uk>
2020-11-23 15:33:10 +0000

    Add config to purge css for tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,6 +10,11 @@ module.exports = {
   //     'nuxt.config.js'
   //   ]
   // },
+  purge: [
+    './src/**/*.html',
+    './src/**/*.vue',
+    './src/**/*.jsx',
+  ],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
